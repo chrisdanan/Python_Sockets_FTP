1a78965bce65c8b87fb32c28dd32d6a0954a72c5
Christopher Danan <chrisddan@gmail.com>
2015-05-01 17:32:34 -0700

    Code 'put' functionality so the client can send files to the server: -Put: client can upload a file to the server -Notes:  *The file can transfer to the server but it does not save - may want to   include save functionality later.  *There is an infinite loop error when the client sends in a non-existent   file name. Mario suggests checking file name against existing files.  *Refactor code later to introduce functions.

--- client.py
+++ client.py
@@ -1,8 +1,53 @@
 # Client code
 
+#Authors:
+#   Mario Andrade
+#   Christopher Dancarlo Danan
+#   Austin Greene
+#   Sarah Lee
+#For:
+#   CPSC 471
+#   Assignment 3
+#Due:
+#   May 1, 2015
+#References:
+#   Professor Gofman's sample codes from Titanium.
+
 from socket import *
 import os
 import sys
+
+
+#Reference: Professor Gofman's sample code.
+# ************************************************
+# Receives the specified number of bytes
+# from the specified socket
+# @param sock - the socket from which to receive
+# @param numBytes - the number of bytes to receive
+# @return - the bytes received
+# *************************************************
+def recvAll(sock, numBytes):
+
+    # The buffer
+    recvBuff = ""
+    
+    # The temporary buffer
+    tmpBuff = ""
+    
+    # Keep receiving till all is received
+    while len(recvBuff) < numBytes:
+        
+        # Attempt to receive bytes
+        tmpBuff =  sock.recv(numBytes)
+        
+        # The other side has closed the socket
+        if not tmpBuff:
+            break
+        
+        # Add the received bytes to the buffer
+        recvBuff += tmpBuff
+    
+    return recvBuff
 
 # Check the arguments passed by command line.
 if len(sys.argv) != 3:
@@ -46,8 +91,77 @@ while True:
                 print (value)
         elif command[0] == "get":
             pass
-        elif command[0] == "push":
-            pass
+        elif command[0] == "put":
+            #Check for correct usage of 'put'
+            if len(command) != 2:
+                print("USAGE put <filename>")
+            else:
+                #Store filename into a variable.
+                fileName = command[1]
+
+                #Open the file.
+                fileObj = open(fileName, "r")
+
+                #The file data.
+                fileData = None
+
+                # Send 'put' command to server.
+                clientSocket.send(command[0])
+
+                #Generate ephemeral port.
+                #Reference: Professor Gofman's example code.
+                dataSocket = socket(AF_INET, SOCK_STREAM)
+                dataSocket.bind(("", 0))
+                print("I chose ephemeral port: ", dataSocket.getsockname()[1])
+
+                #Send ephemeral port number to server.
+                clientSocket.send(str(dataSocket.getsockname()[1]))
+
+                dataSocket.listen(1)
+
+                while 1:
+                    print("Waiting for server connection.")
+
+                    connectionSocket, addr = dataSocket.accept()
+
+                    print("Received connection from the server.")
+
+                    #Read 65536 bytes of data.
+                    fileData = fileObj.read(65536)
+
+                    #Make sure we did not hit EOF.
+                    if fileData:
+
+                        #Get the size of the data read and convert it to string.
+                        dataSizeStr = str(len(fileData))
+
+                        #Prepend 0's to the size string until the size is 10 bytes.
+                        while len(dataSizeStr) < 10:
+                            dataSizeStr = "0" + dataSizeStr
+
+                        #Prepend the size of the data to the file data.
+                        fileData = dataSizeStr + fileData
+
+                        #The number of bytes sent.
+                        numSent = 0
+
+                        #Send the data!
+                        while len(fileData) > numSent:
+                            numSent += connectionSocket.send(fileData[numSent:])
+
+                        #At this point, the client is done sending file data to the server.
+
+                        #Print out file name and number of bytes transferred.
+                        print("Sent " + fileName + " to the server.")
+                        print(numSent, " bytes sent to the server.")
+
+                        #Finished sending the data, so close the data connection.
+                        connectionSocket.close()
+
+                        #Get the connection flag from the server.
+                        connectionFlag = clientSocket.recv(9)
+                        break
+
         elif command[0] == "quit":
             # Send the command to the server.
             clientSocket.send(command[0])
